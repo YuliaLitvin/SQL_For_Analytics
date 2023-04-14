b04c09a0e4f232e3123d0e643a857e3cd1bbac3d
YuliaLitvin <110967564+YuliaLitvin@users.noreply.github.com>
2023-04-14 19:34:42 +0300

    Add files via upload

---
+++ StrataScratch_11.sql
@@ -0,0 +1,10 @@
+--You're given a dataset of health inspections. Count the number of violation in an inspection in 'Roxanne Cafe' for each year.
+--If an inspection resulted in a violation, there will be a value in the 'violation_id' column.
+--Output the number of violations by year in ascending order.
+
+
+select date_part('year', inspection_date) as year, count(violation_id) as n_inspections
+from sf_restaurant_health_violations
+where business_name like 'Roxanne Cafe'
+group by 1
+order by 1;
